66c121ad5fd43d1d65a1fa708fd1a4b4019bb12d
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2022-11-16 15:43:15 +0900

    add jump

--- Unity_2d_Flatformer/Assets/scripts/PlayerMove.cs
+++ Unity_2d_Flatformer/Assets/scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
@@ -21,7 +22,14 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetButtonUp("Horizontal"))
+        //Jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+
+        if (Input.GetButtonUp("Horizontal"))
         {
             //stop speed
             rigid.velocity = new Vector2(rigid.velocity.normalized.x*(0.5f), rigid.velocity.y);
@@ -50,5 +58,19 @@ public class PlayerMove : MonoBehaviour
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x < maxSpeed*(-1))//left
             rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+
+        //Landing Platform
+        //Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+        if(rigid.velocity.y<0)
+        {
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1,LayerMask.GetMask("Platform"));
+            if(rayHit.collider != null)
+            {
+                if (rayHit.distance <= 0.5f)
+                    anim.SetBool("isJumping", false);
+            }
+        }
+
+
     }
 }
